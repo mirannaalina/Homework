d74102a1cdd741b8aeb3651f515d0f53730c62fb
mirannaalina <v_mxa@yahoo.com>
2019-06-21 15:27:33 +0300

    Third homework

---
+++ src/Homework/HWday2/Tema12.java
@@ -0,0 +1,24 @@
+package Homework.HWday2;
+
+public class Tema12 {
+
+    public static void main (String[]args){
+
+        int numere = 5352;
+
+       int  ultimacifra = numere %10;
+
+       int split = numere /1000;
+
+        System.out.println(ultimacifra);
+
+        System.out.println(split);
+    }
+
+    public static void functie (){
+        int unnr =14;
+        while(unnr!=0){
+
+        }
+    }
+}
